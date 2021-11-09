301935de906555dbf415e29224df0712e6effa9b
mathieux-dev <mateusvmourao@gmail.com>
2021-11-09 15:53:28 -0300

    initial commit

---
+++ ByteBank.Forum/ByteBank.Forum/Controllers/ContaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+    }
+}
