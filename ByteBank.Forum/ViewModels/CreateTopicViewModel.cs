497bec21e8a4738b679e7330b63eabac0d2cd8b0
mathieux-dev <mateusvmourao@gmail.com>
2021-11-11 16:52:13 -0300

    adicionando funcionalidade de 'criar topico'

---
+++ ByteBank.Forum/ViewModels/CreateTopicViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ByteBank.Forum.ViewModels
+{
+    public class CreateTopicViewModel
+    {
+        public string Tittle { get; set; }
+        public string Content { get; set; }
+    }
+}
