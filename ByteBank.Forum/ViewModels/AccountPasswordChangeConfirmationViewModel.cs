60b6ec51a5e7e4e0d7d0cde48d3eb75d215cfb6b
mathieux-dev <mateusvmourao@gmail.com>
2021-11-11 16:49:26 -0300

    adicionado esquema de 'esqueci a senha', models, viewModels e views.

---
+++ ByteBank.Forum/ViewModels/AccountPasswordChangeConfirmationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.ViewModels
+{
+    public class AccountPasswordChangeConfirmationViewModel
+    {
+        [HiddenInput(DisplayValue = false)]
+        public string UserId { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
+        public string Token { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Nova Senha")]
+        public string NewPassword { get; set; }
+    }
+}
